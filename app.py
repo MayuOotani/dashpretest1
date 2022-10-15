5f0311f2bf92a9d22007e0ce25657f3666223e7c
al19011 <al19011@shibaura-it.ac.jp>
2022-10-15 22:04:48 +0900

    debug

--- app.py
+++ app.py
@@ -438,4 +438,4 @@ def update_location(pathname):
         return start, text
 
 if __name__ == "__main__":
-    app.run_server(debug=True, host="0.0.0.0", port=19011)
+    app.run_server(debug=False, host="0.0.0.0", port=19011)
